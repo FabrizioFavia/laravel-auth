f553f7183523f943153fa6188d4ed70925e7424e
FabrizioFavia <personal.favia@gmail.com>
2023-07-10 16:30:38 +0200

    created controller and added middleware(auth)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+/* Route::get('/admin', [DashboardController::class, 'index'])->name('admin.dashboard'); */
 
+Route::middleware(['auth'])
+ 	->prefix('admin') //definisce il prefisso "admin/" per le rotte di questo gruppo
+    /* ->namespace('Admin') */
+ 	->name('admin.') //definisce il pattern con cui generare i nomi delle rotte cioè "admin.qualcosa"
+ 	->group(function () {
+ 	
+ 		//Siamo nel gruppo quindi:
+ 		// - il percorso "/" diventa "admin/"
+ 		// - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
+ 		Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    });
+    
 require __DIR__.'/auth.php';
+ 
